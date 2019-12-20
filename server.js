3ee1e3a8bfa5c97e209f2d639c2dfa1ef0fc8878
CTConsulting <ecampuzano@ctconsulting.com.mx>
2019-12-20 10:59:59 -0600

    Hot fix

--- server.js
+++ server.js
@@ -18,188 +18,187 @@ app.get('/getClienteFromPhone/:phone', (req, res)=>{
 
     axios.get(`http://201.149.55.114/WSpetco.asmx/getContact?numtel=${phone}`)
     .then(resPromise=>{
-        if(!(resPromise.data.includes("<Contact>") && resPromise.data.includes("</Contact>"))){
-            res.send({
-                response: "getClienteFromPhone",
-                phone,
-                "IDCli":
-                    ""
-                ,
-                "petcogrooming":
-                    ""
-                ,
-                "petcoach":
-                    ""
-                ,
-                "petconps":
+        let jsonResponse = {
+            response: "getClienteFromPhone",
+            phone,
+            "IDCli":
                 ""
-                ,
-                "petcopedido":
-                    ""
-                ,
-                "petcocupones":
-                    ""
-                ,
-                "apPat":
-                    ""
-                ,
-                "apMat":
-                    ""
-                ,
-                "nombre":
-                    ""
-                ,
-                "gen":
-                    ""
-                ,
-                "address1":
-                    ""
-                ,
-                "address2":
-                    ""
-                ,
-                "estado":
-                    ""
-                ,
-                "municipio":
-                    ""
-                ,
-                "telefono":
-                    ""
-                ,
-                "correo":
-                    ""
-                ,
-                "idMasc1":
-                    ""
-                ,
-                "noMasc1":
-                    ""
-                ,
-                "razMasc1":
-                    ""
-                ,
-                "pesMas1":
-                    ""
-                ,
-                "nacMasc1":
-                    ""
-                ,
-                "sexMasc1":
-                    ""
-                ,
-                "colMasc1":
-                    ""
-                ,
-                "idMasc2":
-                    ""
-                ,
-                "noMasc2":
-                    ""
-                ,
-                "razMasc2":
-                    ""
-                ,
-                "pesMas2":
-                    ""
-                ,
-                "nacMasc2":
-                    ""
-                ,
-                "sexMasc2":
-                    ""
-                ,
-                "colMasc2":
-                    ""
-                ,
-                "idMasc3":
-                    ""
-                ,
-                "noMasc3":
-                    ""
-                ,
-                "razMasc3":
-                    ""
-                ,
-                "pesMas3":
-                    ""
-                ,
-                "nacMasc3":
-                    ""
-                ,
-                "sexMasc3":
-                    ""
-                ,
-                "colMasc3":
-                    ""
-                ,
-                "idMasc4":
-                    ""
-                ,
-                "noMasc4":
-                    ""
-                ,
-                "razMasc4":
-                    ""
-                ,
-                "pesMas4":
-                    ""
-                ,
-                "nacMasc4":
-                    ""
-                ,
-                "sexMasc4":
-                    ""
-                ,
-                "colMasc4":
-                    ""
-                ,
-                "idMasc5":
-                    ""
-                ,
-                "noMasc5":
-                    ""
-                ,
-                "razMasc5":
-                    ""
-                ,
-                "pesMas5":
-                    ""
-                ,
-                "nacMasc5":
-                    ""
-                ,
-                "sexMasc5":
-                    ""
-                ,
-                "colMasc5":
-                    ""
-                
-             });
+            ,
+            "petcogrooming":
+                ""
+            ,
+            "petcoach":
+                ""
+            ,
+            "petconps":
+            ""
+            ,
+            "petcopedido":
+                ""
+            ,
+            "petcocupones":
+                ""
+            ,
+            "apPat":
+                ""
+            ,
+            "apMat":
+                ""
+            ,
+            "nombre":
+                ""
+            ,
+            "gen":
+                ""
+            ,
+            "address1":
+                ""
+            ,
+            "address2":
+                ""
+            ,
+            "estado":
+                ""
+            ,
+            "municipio":
+                ""
+            ,
+            "telefono":
+                ""
+            ,
+            "correo":
+                ""
+            ,
+            "idMasc1":
+                ""
+            ,
+            "noMasc1":
+                ""
+            ,
+            "razMasc1":
+                ""
+            ,
+            "pesMas1":
+                ""
+            ,
+            "nacMasc1":
+                ""
+            ,
+            "sexMasc1":
+                ""
+            ,
+            "colMasc1":
+                ""
+            ,
+            "idMasc2":
+                ""
+            ,
+            "noMasc2":
+                ""
+            ,
+            "razMasc2":
+                ""
+            ,
+            "pesMas2":
+                ""
+            ,
+            "nacMasc2":
+                ""
+            ,
+            "sexMasc2":
+                ""
+            ,
+            "colMasc2":
+                ""
+            ,
+            "idMasc3":
+                ""
+            ,
+            "noMasc3":
+                ""
+            ,
+            "razMasc3":
+                ""
+            ,
+            "pesMas3":
+                ""
+            ,
+            "nacMasc3":
+                ""
+            ,
+            "sexMasc3":
+                ""
+            ,
+            "colMasc3":
+                ""
+            ,
+            "idMasc4":
+                ""
+            ,
+            "noMasc4":
+                ""
+            ,
+            "razMasc4":
+                ""
+            ,
+            "pesMas4":
+                ""
+            ,
+            "nacMasc4":
+                ""
+            ,
+            "sexMasc4":
+                ""
+            ,
+            "colMasc4":
+                ""
+            ,
+            "idMasc5":
+                ""
+            ,
+            "noMasc5":
+                ""
+            ,
+            "razMasc5":
+                ""
+            ,
+            "pesMas5":
+                ""
+            ,
+            "nacMasc5":
+                ""
+            ,
+            "sexMasc5":
+                ""
+            ,
+            "colMasc5":
+                ""
+            
+         }
+        if(!(resPromise.data.includes("<Contact>") && resPromise.data.includes("</Contact>"))){
+            res.send(jsonResponse);
              return;
             }
         parseString(resPromise.data, (err, jsonParsed)=>{
             let parsed = jsonParsed.ArrayOfContact.Contact;
-            let jsonResponse = { };
             if(!parsed || parsed === {} || parsed === [] || err)
-                res.send({
-                    response: "getClienteFromPhone",
-                    phone
-                });
+                res.send(jsonResponse);
+            
+            
+
+
             for(var obj of parsed){
+                let jsonString =  JSON.stringify(obj).replace(/[\[\]]/gi, "");
+                obj = JSON.parse(jsonString);
                 for(var property in obj){
                     if(property.includes('petco'))
-                        for(var i = 0; i < obj[property].length; i++)
-                            obj[property][i] = "https://".concat(obj[property][i]);
+                        obj[property] = "https://".concat(obj[property]);
 
                     jsonResponse[property] = obj[property];
                 }
-            }
+            } 
             
-            jsonResponse.response = "getClienteFromPhone";
-            jsonResponse.phone = phone;
-            let jsonString = JSON.stringify(jsonResponse).replace(/[\[\]]/gi, "");
-            jsonResponse = JSON.parse(jsonString);
+            
             res.send(jsonResponse);
         })
     })
